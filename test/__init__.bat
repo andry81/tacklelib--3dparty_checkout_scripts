8c74608f7629db53056411decdd37c9f915ac2bc
andry81 <andry@inbox.ru>
2020-02-03 06:58:25 +0000

    * changed: tacklelib checkout scripts extracted from the `tacklelib--3dparty` project
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/3dparty_checkout_scripts/trunk@2 a5ac87a0-aa4a-4415-8cd9-e40022e3bdf9

---
+++ test/__init__.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call "%%~dp0..\__init__.bat" || exit /b
+
+set "CONFIGURE_DIR=%~dp0"
+set "CONFIGURE_DIR=%CONFIGURE_DIR:~0,-1%"
+
+if exist "%CONFIGURE_DIR%/config.vars.in" (
+  call "%%CONTOOLS_ROOT%%\load_config.bat" "%%CONFIGURE_DIR%%" "config.vars" || exit /b
+)
